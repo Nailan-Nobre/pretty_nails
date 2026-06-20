5393abee624bbec55916b1349b05b4c5f940d1e6
Nailan-Nobre <nailannobre22@gmail.com>
2026-06-20 15:51:19 -0300

    ...

--- back_end/src/app.js
+++ back_end/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const agendamentoController = require('./controllers/agendamentoController');
@@ -13,13 +12,9 @@ const analiseRoutes = require('./routes/analiseRoutes');
 const app = express();
 
 // Middlewares
-const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:3000';
-app.use(cors({ origin: allowedOrigin }));
+app.use(cors());
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
-
-const frontendRoot = path.resolve(__dirname, '../../Pretty-Nails');
-app.use(express.static(frontendRoot));
 
 // Middleware de logging para debug
 app.use((req, res, next) => {
@@ -33,15 +28,6 @@ app.use((req, res, next) => {
 // Rotas públicas
 app.use('/auth', authRoutes);
 app.use('/feedback', feedbackRoutes); // Rota pública para visualizar feedbacks
-
-// Página pública de agendamento por slug
-app.get('/agendamento', (req, res) => {
-  res.sendFile(path.join(frontendRoot, 'agendamento', 'agendamento.html'));
-});
-
-app.get('/agendamento/:slug', (req, res) => {
-  res.sendFile(path.join(frontendRoot, 'agendamento', 'agendamento.html'));
-});
 
 app.post('/api/agendamentos/public', agendamentoController.criarAgendamento);
 
--- front_end/lib/services/api_service.dart
+++ front_end/lib/services/api_service.dart
@@ -5,7 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class ApiService {
   static const String baseUrl = String.fromEnvironment(
     'BACKEND_URL',
-    defaultValue: 'http://localhost:3000',
+    defaultValue: 'https://pretty-nails-do11.vercel.app/',
   );
 
   static Future<String?> getToken() async {
